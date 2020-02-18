b7089e460b02ebadb598dd27fc65fb7b8267cdca
Leonard Sheng Sheng Lee <leonard.sheng.sheng.lee@gmail.com>
2020-02-18 14:41:05 +0100

    chore: review

--- examples/simple2/main.tf
+++ examples/simple2/main.tf
@@ -6,13 +6,13 @@ resource "aws_eip" "wireguard" {
 }
 
 module "wireguard" {
-  source               = "git@github.com:sheeeng/terraform-aws-wireguard.git?ref=chore/review"
+  source               = "git@github.com:jmhale/terraform-wireguard.git?ref=v1.0.0"
   ssh_key_id           = "APKAU5XX26VWYZAYWBGR"
   vpc_id               = "vpc-0123456789"
   subnet_ids           = ["subnet-0123456789"]
   eip_id               = "${aws_eip.wireguard.id}"
-  wireguard_server_net = "192.168.2.1/24" # client IPs MUST exist in this net
-  wireguard_client_public_keys = [
+  wg_server_net = "192.168.2.1/24" # client IPs MUST exist in this net
+  wg_client_public_keys = [
     { "192.168.2.2/32" = "U1t4kAJxdUWWlAqZOknG0m9fJf22T6kpzQJxNYjgpmE=" },   # make sure these are correct
     { "192.168.2.3/32" = "AgjIG8xLfsQdvd+OUxBiVq47Z5JsQkBmYywhHme0ZFc=" },   # wireguard is sensitive
     { "192.168.2.255/32" = "9b9jliJ37/E2spqz2dxHzb79pwaK+Ln8nK3//RGY3kg=" }, # to bad configuration
